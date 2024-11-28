21c2ebeda0fab221275d9329dd59244aed992920
Graza <code@graza.io>
2024-11-28 12:29:32 +0000

    refactor: updated to use struct columns (#11)

--- rows/activity_log.go
+++ rows/activity_log.go
@@ -9,37 +9,41 @@ import (
 type ActivityLog struct {
 	enrichment.CommonFields
 
-	AuthorizationAction            *string             `json:"authorization_action"`
-	AuthorizationRole              *string             `json:"authorization_role"`
-	AuthorizationScope             *string             `json:"authorization_scope"`
-	Caller                         *string             `json:"caller"`
-	Category                       *string             `json:"category"`
-	Claims                         *map[string]*string `json:"claims,omitempty" parquet:"type=JSON"`
-	CorrelationID                  *string             `json:"correlation_id"`
-	Description                    *string             `json:"description"`
-	EventDataID                    *string             `json:"event_data_id"`
-	EventName                      *string             `json:"event_name"`
-	EventTimestamp                 *time.Time          `json:"event_timestamp"`
-	HttpRequestInfoClientIpAddress *string             `json:"http_request_info_client_ip_address"`
-	HttpRequestInfoClientRequestId *string             `json:"http_request_info_client_request_id"`
-	HttpRequestInfoMethod          *string             `json:"http_request_info_method"`
-	HttpRequestInfoUri             *string             `json:"http_request_info_uri"`
-	ID                             *string             `json:"id"`
-	Level                          *string             `json:"level"`
-	OperationID                    *string             `json:"operation_id"`
-	OperationName                  *string             `json:"operation_name"`
-	Properties                     *map[string]*string `json:"properties,omitempty" parquet:"type=JSON"`
-	ResourceGroupName              *string             `json:"resource_group_name"`
-	ResourceID                     *string             `json:"resource_id"`
-	ResourceProviderName           *string             `json:"resource_provider_name"`
-	ResourceType                   *string             `json:"resource_type"`
-	Status                         *string             `json:"status"`
-	SubStatus                      *string             `json:"sub_status"`
-	SubmissionTimestamp            *time.Time          `json:"submission_timestamp"`
-	SubscriptionID                 *string             `json:"subscription_id"`
-	TenantID                       *string             `json:"tenant_id"`
+	Authorization        *ActivityLogAuthorization `json:"authorization,omitempty"`
+	Caller               *string                   `json:"caller"`
+	Category             *string                   `json:"category"`
+	Claims               *map[string]*string       `json:"claims,omitempty" parquet:"type=JSON"`
+	CorrelationID        *string                   `json:"correlation_id"`
+	Description          *string                   `json:"description"`
+	EventDataID          *string                   `json:"event_data_id"`
+	EventName            *string                   `json:"event_name"`
+	EventTimestamp       *time.Time                `json:"event_timestamp"`
+	HttpRequest          *ActivityLogHttpRequest   `json:"http_request,omitempty"`
+	ID                   *string                   `json:"id"`
+	Level                *string                   `json:"level"`
+	OperationID          *string                   `json:"operation_id"`
+	OperationName        *string                   `json:"operation_name"`
+	Properties           *map[string]*string       `json:"properties,omitempty" parquet:"type=JSON"`
+	ResourceGroupName    *string                   `json:"resource_group_name"`
+	ResourceID           *string                   `json:"resource_id"`
+	ResourceProviderName *string                   `json:"resource_provider_name"`
+	ResourceType         *string                   `json:"resource_type"`
+	Status               *string                   `json:"status"`
+	SubStatus            *string                   `json:"sub_status"`
+	SubmissionTimestamp  *time.Time                `json:"submission_timestamp"`
+	SubscriptionID       *string                   `json:"subscription_id"`
+	TenantID             *string                   `json:"tenant_id"`
 }
 
-func NewActivityLog() *ActivityLog {
-	return &ActivityLog{}
+type ActivityLogAuthorization struct {
+	Action *string `json:"action"`
+	Scope  *string `json:"scope"`
+	Role   *string `json:"role"`
+}
+
+type ActivityLogHttpRequest struct {
+	ClientIpAddress *string `json:"client_ip_address,omitempty"`
+	ClientRequestId *string `json:"client_request_id,omitempty"`
+	Method          *string `json:"method,omitempty"`
+	Uri             *string `json:"uri,omitempty"`
 }
